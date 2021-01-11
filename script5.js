4a841a283e93bfb4697483e530ec28c690254b45
VeMartins <66840333+VeMartins@users.noreply.github.com>
2021-01-11 21:19:11 +0100

    refactoring

--- script5.js
+++ script5.js
@@ -2,148 +2,113 @@ const note = document.getElementById("newNote");
 const notesMainDiv = document.querySelector(".notePrint");
 let notesArr = []; // array for all the noteDiv(noteBox)
 
-const popContent = document.getElementById("popContent");	
+const popContent = document.getElementById("popContent");
 const closePop = document.getElementById("popCloseBtn");
-popContent.appendChild(closePop);	
-
-
+popContent.appendChild(closePop);
 
 const addNote = () => {
+  createNoteDiv(note.value);
+  saveLocalNote(note.value);
+  note.value = "";
+};
+const createNoteDiv = (data) => {
+  //creating individual note
+  let noteBox = document.createElement("div");
+  noteBox.setAttribute("class", "noteBox");
+  notesMainDiv.appendChild(noteBox);
+  notesArr.push(noteBox);
 
-		//Creating notesBox divs(+ class) and appending text from TextArea
-	let noteBox = document.createElement("div");
-	noteBox.setAttribute("class", "noteBox");
-	notesMainDiv.appendChild(noteBox);
-	notesArr.push(noteBox);
+  let parag = document.createElement("p");
+  parag.setAttribute("class", "noteTxt");
+  let txt = document.createTextNode(data);
+  parag.appendChild(txt);
+  noteBox.appendChild(parag);
+  //creating remove note button
+  let closeBtn = document.createElement("span");
+  closeBtn.setAttribute("id", "closeBtn");
+  closeBtn.innerHTML = "&times;";
+  noteBox.appendChild(closeBtn);
 
-	let parag = document.createElement("p");
-	parag.setAttribute("class", "noteTxt"); 
-	let txt = document.createTextNode(note.value); 
-	parag.appendChild(txt);
-	noteBox.appendChild(parag);
-	
-	 saveLocalNote(note.value);
+  //creating details button
+  let detailBtn = document.createElement("button");
+  detailBtn.setAttribute("id", "detailBtn");
+  let btnTxt = document.createTextNode("View details");
+  detailBtn.appendChild(btnTxt);
+  noteBox.appendChild(detailBtn);
 
-		//creating close button for notesBox (+ class) 
-	let closeBtn = document.createElement("span");
-	closeBtn.setAttribute("id", "closeBtn");
-	closeBtn.innerHTML = '&times;';
-	noteBox.appendChild(closeBtn);
-
-		//creating details button
-	let detailBtn = document.createElement("button"); 
-	detailBtn.setAttribute("id", "detailBtn");
-	let btnTxt = document.createTextNode("View details");
-	detailBtn.appendChild(btnTxt);
-	noteBox.appendChild(detailBtn);
-
-	note.value = "";// removes input text from textarea 
-
-	eventDelBtn(closeBtn);
-	detailBtn.addEventListener("click", popTheBox);
+  eventDelBtn(closeBtn);
+  detailBtn.addEventListener("click", popTheBox);
 };
 
 const eventDelBtn = (closeBtn) => {
-	closeBtn.addEventListener("click", closeNote);	
+  closeBtn.addEventListener("click", closeNote);
 };
 
 const openBox = (detailBtn) => {
-		popContent.style.display = "grid";
-		detailBtn.stopPropagation();
+  popContent.style.display = "grid";
+  detailBtn.stopPropagation();
 };
 
-// Event Listeners 
+// Event Listeners
 const popTheBox = (detailBtn) => {
+  if (closePop.nextElementSibling) {
+    closePop.nextElementSibling.remove();
+  } // to remove 1st cloned <p> if detailBtn is clicked twice(if popBox is not closed 1st)
+  let targt = detailBtn.target.parentNode.firstChild; // to get <p> of the notebox
+  let cln = targt.cloneNode(true); // clones <p> from notebox
+  cln.setAttribute("class", "clone");
+  popContent.appendChild(cln);
 
-	if (closePop.nextElementSibling) {
-	closePop.nextElementSibling.remove();
-	} // to remove 1st cloned <p> if detailBtn is clicked twice(if popBox is not closed 1st)
-	let targt = detailBtn.target.parentNode.firstChild; // to get <p> of the notebox
-	let cln = targt.cloneNode(true); // clones <p> from notebox
-	cln.setAttribute("class", "clone");
-	popContent.appendChild(cln);
-
-	openBox(detailBtn);
-	closePop.addEventListener("click", closePopBox);
-};	
+  openBox(detailBtn);
+  closePop.addEventListener("click", closePopBox);
+};
 
 const closeNote = (closeBtn) => {
-	closeBtn.stopPropagation();
-	let boxNote = closeBtn.target.parentNode; //targets noteBox (parentNode of closeBtn)
-	removeLocalNotes(boxNote);
-	boxNote.parentNode.removeChild(boxNote);
-}
-
-const closePopBox = () => {
-	let clone = document.querySelector(".clone"); //.clone created in popTheBox event listener
-	clone.remove(); // removes cloned <p> uppon closing popbox so it won't show previous cloned <p>.
-	popContent.style.display = "none";
+  closeBtn.stopPropagation();
+  let boxNote = closeBtn.target.parentNode; //targets noteBox (parentNode of closeBtn)
+  removeLocalNotes(boxNote);
+  boxNote.parentNode.removeChild(boxNote);
 };
 
-document.addEventListener('DOMContentLoaded', getNotes);
+const closePopBox = () => {
+  let clone = document.querySelector(".clone"); //.clone created in popTheBox event listener
+  clone.remove(); // removes cloned <p> uppon closing popbox so it won't show previous cloned <p>.
+  popContent.style.display = "none";
+};
+
+document.addEventListener("DOMContentLoaded", getNotes);
 // Saving to LocalStorage
 
-function saveLocalNote(data){
-	//check if there it is already in localstorage
-	let notes;
-	if(localStorage.getItem('notes') === null) {
-		notes = [];
-	} else {
-		notes = JSON.parse(localStorage.getItem('notes'));
-	}
-	notes.push(data);
-	localStorage.setItem("notes", JSON.stringify(notes));
+function saveLocalNote(data) {
+  //check if there it is already in localstorage
+  let notes;
+  if (localStorage.getItem("notes") === null) {
+    notes = [];
+  } else {
+    notes = JSON.parse(localStorage.getItem("notes"));
+  }
+  notes.push(data);
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
 
 function getNotes() {
-	let notes;
-	if(localStorage.getItem('notes') === null) {
-		notes = [];
-	} else {
-		notes = JSON.parse(localStorage.getItem('notes'));
-	}
-	notes.forEach(function(data){
-		let noteBox = document.createElement("div");
-		noteBox.setAttribute("class", "noteBox");
-		notesMainDiv.appendChild(noteBox);
-		notesArr.push(noteBox);
-
-		let parag = document.createElement("p");
-		parag.setAttribute("class", "noteTxt"); 
-		let txt = document.createTextNode(data); 
-		parag.appendChild(txt);
-		noteBox.appendChild(parag);
-		
-		
-
-			//creating close button for notesBox (+ class) 
-		let closeBtn = document.createElement("span");
-		closeBtn.setAttribute("id", "closeBtn");
-		closeBtn.innerHTML = '&times;';
-		noteBox.appendChild(closeBtn);
-
-			//creating details button
-		let detailBtn = document.createElement("button"); 
-		detailBtn.setAttribute("id", "detailBtn");
-		let btnTxt = document.createTextNode("View details");
-		detailBtn.appendChild(btnTxt);
-		noteBox.appendChild(detailBtn);
-
-		eventDelBtn(closeBtn);
-		detailBtn.addEventListener("click", popTheBox);
-	});
+  let notes;
+  if (localStorage.getItem("notes") === null) {
+    notes = [];
+  } else {
+    notes = JSON.parse(localStorage.getItem("notes"));
+  }
+  notes.forEach((data) => createNoteDiv(data));
 }
 
 function removeLocalNotes(data) {
-	let notes;
-	if(localStorage.getItem('notes') === null) {
-		notes = [];
-	} else {
-		notes = JSON.parse(localStorage.getItem('notes'));
-	}
-	const dataIndex = data.children[0].innerText;
-	notes.splice(notes.indexOf(dataIndex), 1);
-	localStorage.setItem("notes", JSON.stringify(notes));
-
-	
+  let notes;
+  if (localStorage.getItem("notes") === null) {
+    notes = [];
+  } else {
+    notes = JSON.parse(localStorage.getItem("notes"));
+  }
+  const dataIndex = data.children[0].innerText;
+  notes.splice(notes.indexOf(dataIndex), 1);
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
